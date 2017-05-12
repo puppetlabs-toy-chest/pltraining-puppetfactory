114bc0f118bab9b9af4f2cf8992fdd16db8cacc2
Greg Larkin <greg.larkin@puppet.com>
2017-05-11 17:00:33 -0700

    Remove build-time mountpoints. These directories will be bound to the container at runtime.

--- manifests/centosimage.pp
+++ manifests/centosimage.pp
@@ -14,38 +14,6 @@ class puppetfactory::centosimage (
     require => Class['docker'],
   }
 
-  # Mount the mirror directory because docker doesn't follow symlinks
-  # mount --bind /var/yum/mirror mirror
-  file {'/var/docker/centosagent/mirror':
-    ensure => directory,
-    before => Mount['/var/docker/centosagent/mirror'],
-  }
-
-  mount {'/var/docker/centosagent/mirror':
-    ensure  => mounted,
-    atboot  => true,
-    device  => '/var/yum/mirror',
-    fstype  => 'xfs',
-    options => 'bind',
-    before  => Docker::Image['centosagent']
-  }
-
-  # Mount the gem cache directory because docker doesn't follow symlinks
-  # mount --bind /var/cache/rubygems rubygems
-  file {'/var/docker/centosagent/rubygems':
-    ensure => directory,
-    before => Mount['/var/docker/centosagent/rubygems'],
-  }
-
-  mount {'/var/docker/centosagent/rubygems':
-    ensure  => mounted,
-    atboot  => true,
-    device  => '/var/cache/rubygems',
-    fstype  => 'xfs',
-    options => 'bind',
-    before  => Docker::Image['centosagent']
-  }
-
   file { '/var/docker/centosagent/Dockerfile':
     ensure  => present,
     content => epp('puppetfactory/centos.dockerfile.epp',{
@@ -58,4 +26,3 @@ class puppetfactory::centosimage (
     docker_dir => '/var/docker/centosagent/',
   }
 }
-
